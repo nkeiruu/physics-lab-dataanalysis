34520cc338ca4e2499e3315e741e7ba61f0564d6
nkeiruu <nkeiruubadike@gmail.com>
2021-03-22 00:48:27 -0400

    Can insert array into given formula and compute new array and write to file

--- Derived_Array_Data.py
+++ Derived_Array_Data.py
@@ -6,8 +6,6 @@ from scipy.optimize import curve_fit
 plt.rcParams["font.family"] = "serif"
 
 
-
-
 def parse_file(file_name, header=True):
     raw_data_1 = np.array([])
     raw_data_2 = np.array([])
@@ -21,11 +19,30 @@ def parse_file(file_name, header=True):
             raw_data_2 = np.append(raw_data_2, float(split_line[1]))
     return [raw_data_1, raw_data_2]
 
+#calculate the square of all array elements
 def new_array_formula(raw_data_1):
     new_data = np.array([])
-    new_data = raw_data_1*2
+    new_data = np.sin(raw_data_1) + 3 #Input formula here
     return(new_data)
 
+def write_to_file(new_data, file_name_write):
+    output = "" #A place to put the data 
+    with open(file_name_write, "w+") as file:
+        for i in new_data:
+            output += str(i) + "\n" #Convert data to string and put in holder
+        file.write(output)
+    file.close()
+    print(f'The computed data has been written to {file_name_write}')
 
 file_name = 'PHY251Lab2ElectronCharge.csv'
-print(new_array_formula)
+file_name_write = 'TrialComputedData.csv'
+#Calling parse file function to load the data and store in raw_data
+raw_data = parse_file(file_name, header = False)
+
+#Call new array formula
+computed_data = new_array_formula(raw_data[0])
+
+print(computed_data)
+
+write_to_file(computed_data, file_name_write)
+
