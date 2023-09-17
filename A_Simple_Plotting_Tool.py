287075c7625a862dbbd4bc7140e6fa30caef4259
nkeiruu <nkeiruubadike@gmail.com>
2023-09-17 00:06:02 -0400

    Rewrote as a command line scipt

--- A_Simple_Plotting_Tool.py
+++ A_Simple_Plotting_Tool.py
@@ -9,17 +9,19 @@ def simple_plot(x_start, x_end, f_x,x_label,y_label):
     xlist = np.array(np.arange(x_start,x_end,0.1))
     
     # input Fuction
-    f_x= 1/(np.exp(1/xlist )-1)
+    #f_x= 1/(np.exp(1/xlist )-1)
+    ret_f_x= eval(f_x)
+          
     
     
     #Plotting
     plt.figure(figsize=(10,8))
-    plt.plot(xlist, f_x, marker = "o")
+    plt.plot(xlist, ret_f_x, marker = "o")
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.show()
 
-    if __name__ == "__main__":
-        import sys
-        print(f"Enter x axis start, x axis end, numpy function expression (use xlist as x), x axis label, y axis label")
-        simple_plot(float(sys.argv[1]), float(sys.argv[2]),sys.argv[3],sys.argv[4], sys.argv[5]  )
+if __name__ == "__main__":
+    import sys
+    #print(sys.argv)
+    simple_plot(float(sys.argv[1]), float(sys.argv[2]),sys.argv[3],sys.argv[4], sys.argv[5])
